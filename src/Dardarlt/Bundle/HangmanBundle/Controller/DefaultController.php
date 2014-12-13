c78015b57a8c2b20eab935bab6d13eec1f5662e2
Darius <darius@foxrate.de>
2014-12-13 12:43:36 +0200

    Fix PSR-2. Enable app_dev in vagrant box. Create api routing

--- src/Dardarlt/Bundle/HangmanBundle/Controller/DefaultController.php
+++ src/Dardarlt/Bundle/HangmanBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace Dardarlt\Bundle\HangmanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        return new Response('test');
         return $this->render('DardarltHangmanBundle:Default:index.html.twig', array('name' => $name));
     }
 }
